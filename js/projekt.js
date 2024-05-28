fb2d8e73624f158d1851141d28406a3367c4a764
Simon Gartzke <simon.gartzke@icloud.com>
2024-05-28 21:31:54 +0200

    projekt nur zum test

--- js/projekt.js
+++ js/projekt.js
@@ -1,6 +1,9 @@
 class Projekt {
-    titel
-    kurzbeschreibung
-    pfadZuProjektLogo
-    startDatum
+    constructor(titel, kurzbeschreibung, pfadZuProjektLogo, startDatum) {
+        this.titel = titel 
+        this.kurzbeschreibung = kurzbeschreibung
+        this.pfadZuProjektLogo = pfadZuProjektLogo
+        this.startDatum = startDatum
+    }
+  
 }

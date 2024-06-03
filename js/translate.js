df0c9df0fbc2fe39247b54d2b62252d6bc3228a1
derPascalD <pascal_daniel@gmx.de>
2024-06-03 19:00:18 +0200

    delete Commits

--- js/translate.js
+++ js/translate.js
@@ -14,50 +14,15 @@ class Translate {
 
 
 const enMap = new Map([
-    ["text-title", "Titel"],
-    ["text-label-Benutzername", "Benutzername"],
-    ["Benutzername", "Benutzername"],
-    ["Password", "Passwort"],
-    ["text-a-menu-one", ""],
-    ["text-a-menu-two", ""],
-    ["text-h1-Zeile1", ""],
-    ["text-h1-Zeile2", ""],
-    ["text-h1-question-one", ""],
-    ["text-p-answer-one", ""],
-    ["text-h1-title-one", ""],
-    ["text-a-projekt-one", ""],
-    ["text-a-projekt-two", ""],
-    ["text-a-projekt-three", ""],
-    ["text-a-impressum", "Impressum"],
-    ["text-a-datenschutz", "Datenschutz"],
-    ["text-a-haftung", "Haftung"],
-    ["text-a-scroll-top", "Nach oben"],
-    ["text-p-copyright", "Copyright"]
+/* ["title", "Titel"],
+    ["",""],
+
+
+*/ 
 ]);
 
-/*
-text-title
-text-label-Benutzername
-Benutzername
-text-label-password
-Password
-text-a-menu-one
-text-a-menu-two
-text-h1-Zeile1
-text-h1-Zeile2
-text-h1-question-one
-text-p-answer-one
-text-h1-title-one
-text-a-projekt-one
-text-a-projekt-two
-text-a-projekt-three
-text-a-impressum
-text-a-datenschutz
-text-a-haftung
-text-a-scroll-top
-text-p-copyright
-*/
 
-let translator = new Translate(medics)
+
+let translator = new Translate(enMap)
 
 translator.translate()
